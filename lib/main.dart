6f286fed83b8b1e7a586610de00b28f1f721b176
Teera1995 <teera@frontware.co.th>
2024-08-15 09:38:26 +0700

    separate go router

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:phone_book/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:phone_book/function/auth_checker.dart';
-import 'package:phone_book/function/authentication.dart';
-import 'package:phone_book/screens/login.dart';
-import 'package:phone_book/screens/profile.dart';
-import 'package:phone_book/screens/register.dart';
+import 'package:phone_book/router/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,42 +14,13 @@ void main() async {
   runApp(const MyApp());
 }
 
-final GoRouter _router = GoRouter(routes: <RouteBase>[
-  GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const AuthChecker();
-      }),
-  GoRoute(
-      path: '/login',
-      builder: (BuildContext context, GoRouterState state) {
-        return const LoginScreen();
-      }),
-  GoRoute(
-      path: '/register',
-      builder: (BuildContext context, GoRouterState state) {
-        return const RegisterScreen();
-      }),
-  GoRoute(
-      path: '/profile',
-      redirect: (context, state) {
-        if (UserRepository.instance.isLoggedIn()) {
-          return null;
-        }
-        return '/login';
-      },
-      builder: (BuildContext context, GoRouterState state) {
-        return const ProfileScreen();
-      }),
-]);
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: _router,
+      routerConfig: router,
     );
   }
 }
---
+++ lib/router/router.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:phone_book/function/auth_checker.dart';
+import 'package:phone_book/function/authentication.dart';
+import 'package:phone_book/screens/login.dart';
+import 'package:phone_book/screens/profile.dart';
+import 'package:phone_book/screens/register.dart';
+
+final GoRouter router = GoRouter(routes: <RouteBase>[
+  GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const AuthChecker();
+      }),
+  GoRoute(
+      path: '/login',
+      builder: (BuildContext context, GoRouterState state) {
+        return const LoginScreen();
+      }),
+  GoRoute(
+      path: '/register',
+      builder: (BuildContext context, GoRouterState state) {
+        return const RegisterScreen();
+      }),
+  GoRoute(
+      path: '/profile',
+      redirect: (context, state) {
+        if (UserRepository.instance.isLoggedIn()) {
+          return null;
+        }
+        return '/login';
+      },
+      builder: (BuildContext context, GoRouterState state) {
+        return const ProfileScreen();
+      }),
+]);
