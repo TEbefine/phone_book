7a4e5b1b6e1d980ec1ab6bd9b6413066340c0306
Teera1995 <teera@frontware.co.th>
2024-08-19 11:12:14 +0700

    fist prepare

---
+++ lib/cubit/profile_photo_state.dart
@@ -0,0 +1,10 @@
+part of 'profile_photo_cubit.dart';
+
+sealed class ProfilePhotoState extends Equatable {
+  const ProfilePhotoState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class ProfilePhotoInitial extends ProfilePhotoState {}
