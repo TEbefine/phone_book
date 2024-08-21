6a1a80835ead8b0124ac87d025eb729228c5abd8
Teera1995 <teera@frontware.co.th>
2024-08-21 11:03:48 +0700

    prepar to merge to main

--- lib/screens/widgets/profile_widgets/profile_layout.dart
+++ lib/screens/widgets/profile_widgets/profile_layout.dart
@@ -1,9 +1,3 @@
-import 'dart:io';
-
-import 'package:file_picker/file_picker.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:phone_book/cubit/login_out_cubit/login_out_cubit.dart';
@@ -23,7 +17,6 @@ class _ProfileLayoutState extends State<ProfileLayout> {
 
   @override
   Widget build(BuildContext context) {
-    print(UserRepository.instance.user?.photoURL);
     return Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -33,9 +26,6 @@ class _ProfileLayoutState extends State<ProfileLayout> {
             backgroundImage: NetworkImage(UserRepository
                     .instance.user?.photoURL ??
                 'https://png.pngtree.com/thumb_back/fh260/background/20220904/pngtree-side-profile-of-japanese-monkey-cute-snow-pool-photo-image_22752788.jpg'),
-            onBackgroundImageError: (exception, stackTrace) {
-              print('Error loading image: $exception');
-            },
           ),
           const SizedBox(height: 10.0),
           Text(
@@ -45,16 +35,6 @@ class _ProfileLayoutState extends State<ProfileLayout> {
               onPressed: () {
                 context.read<UpdatePhotoCubit>().changeUserProfilePicture();
               },
-              // () async {
-              //   // ให้ผู้ใช้เลือกและครอปรูปภาพ (หรือใช้งาน code selectAndCropFile)
-              //   final result = await FilePicker.platform.pickFiles();
-              //   if (result != null) {
-              //     final fileBytes = result.files.first.bytes;
-              //     final userId = FirebaseAuth.instance.currentUser?.uid ?? '';
-
-              //     await changeUserProfilePicture(fileBytes!, userId);
-              //   }
-              // },
               child: const Text('Upload Image')),
           const SizedBox(height: 30.0),
           Row(
@@ -107,51 +87,5 @@ class _ProfileLayoutState extends State<ProfileLayout> {
             ],
           )
         ]);
-  }
-
-  Future<String?> uploadProfilePicture(
-      Uint8List fileBytes, String userId) async {
-    try {
-      final storageRef = FirebaseStorage.instance.ref().child(userId);
-
-      final metadata = SettableMetadata(contentType: 'image/jpeg');
-      await storageRef.putData(fileBytes, metadata);
-      final downloadUrl = await storageRef.getDownloadURL();
-      return downloadUrl; // ส่งคืน URL ของรูปภาพที่อัปโหลด
-    } catch (e) {
-      print('Error occurred: $e');
-      return null;
-    }
-  }
-
-  Future<void> updateProfilePicture(String downloadUrl) async {
-    try {
-      final _user = await FirebaseAuth.instance.currentUser;
-      if (_user != null) {
-        await _user.updatePhotoURL(downloadUrl);
-        await _user.reload();
-        print('update profile picture successful');
-      }
-    } catch (e) {
-      print('Error update the profile picture: $e');
-    }
-  }
-
-  Future<void> changeUserProfilePicture(
-      Uint8List fileBytes, String userId) async {
-    // อัปโหลดรูปภาพไปยัง Firebase Storage
-    try {
-      String? downloadUrl = await uploadProfilePicture(fileBytes, userId);
-
-      if (downloadUrl != null) {
-        // อัปเดต URL รูปภาพใน Firebase Authentication
-        await updateProfilePicture(downloadUrl);
-        print('Able to upload');
-      } else {
-        print('Unable to upload');
-      }
-    } catch (e) {
-      print('Error changing the profile picture: $e');
-    }
   }
 }
