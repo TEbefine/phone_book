2c42ea598a06fd6d4baa0a28ded7a7ca3a2a688c
Teera1995 <teera@frontware.co.th>
2024-09-02 13:21:38 +0700

    add new nav

--- lib/screens/new_homepage.dart
+++ lib/screens/new_homepage.dart
@@ -44,6 +44,7 @@ class _NavigationRailExampleState extends State<NavigationRailExample> {
   ];
 
   int _selectedIndex = 0;
+  int _hoveredIndex = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -73,14 +74,266 @@ class _NavigationRailExampleState extends State<NavigationRailExample> {
         children: [
           if (MediaQuery.of(context).size.width >= 640)
             NavigationRail(
+                leading: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    Row(
+                      children: [
+                        Image.network(
+                          "https://ml.globenewswire.com/Resource/Download/27311aae-6eed-48ac-900c-7d107394d35a",
+                          width: 85,
+                        ),
+                        const SizedBox(
+                          width: 120,
+                        ),
+                        IconButton(
+                          icon: const Icon(
+                            Icons.notifications,
+                            color: Colors.grey,
+                          ),
+                          onPressed: () {
+                            // Handle menu button press
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 30.0),
+                    Container(
+                      height: 2.0, // Height of the underline
+                      color: Colors.black,
+                      // Color of the underline
+                    ),
+                  ],
+                ),
+                backgroundColor: Colors.white,
+                useIndicator: false,
+                indicatorColor:
+                    Colors.transparent, // Makes the indicator invisible
+                indicatorShape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(0), // Ensures no visible shape
+                ),
+                selectedIconTheme: IconThemeData(
+                  color: Colors.indigo[500],
+                ),
+                unselectedIconTheme: const IconThemeData(
+                  color: Colors.grey,
+                  size: 24,
+                ),
+                minWidth: 300,
                 onDestinationSelected: (int index) {
                   setState(() {
                     _selectedIndex = index;
                   });
                 },
-                destinations: const [
+                destinations: [
                   NavigationRailDestination(
-                      icon: Icon(Icons.home), label: Text('Home')),
+                    icon: MouseRegion(
+                      onEnter: (_) {
+                        setState(() {
+                          _hoveredIndex = 0;
+                        });
+                      },
+                      onExit: (_) {
+                        setState(() {
+                          _hoveredIndex = -1;
+                        });
+                      },
+                      child: Container(
+                        width: 250,
+                        decoration: BoxDecoration(
+                          color: _selectedIndex == 0
+                              ? Colors.transparent
+                              : (_hoveredIndex == 0
+                                  ? Colors.grey.withOpacity(0.15)
+                                  : Colors.transparent),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Icon(Icons.home),
+                            const SizedBox(width: 10.0),
+                            Text(
+                              'Home',
+                              style: TextStyle(
+                                color: _selectedIndex == 0
+                                    ? Colors.indigo[500]
+                                    : Colors.grey,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w300,
+                                fontSize: 12,
+                                height: 1.6667,
+                              ),
+                            ),
+                            const SizedBox(width: 125.0),
+                            const Icon(
+                              Icons.arrow_forward_ios,
+                              size: 15.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: MouseRegion(
+                      onEnter: (_) {
+                        setState(() {
+                          _hoveredIndex = 0;
+                        });
+                      },
+                      onExit: (_) {
+                        setState(() {
+                          _hoveredIndex = -1;
+                        });
+                      },
+                      child: Container(
+                        width: 250,
+                        decoration: BoxDecoration(
+                          color: _selectedIndex == 0
+                              ? Colors.transparent
+                              : (_hoveredIndex == 0
+                                  ? Colors.grey.withOpacity(0.15)
+                                  : Colors.transparent),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.home),
+                            const SizedBox(width: 10.0),
+                            Text(
+                              'Projects',
+                              style: TextStyle(
+                                color: _selectedIndex == 0
+                                    ? Colors.indigo[500]
+                                    : Colors.grey,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w300,
+                                fontSize: 12,
+                                height: 1.6667,
+                              ),
+                            ),
+                            const SizedBox(width: 125.0),
+                            const Icon(
+                              Icons.arrow_forward_ios,
+                              size: 15.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    label: Text('Projects'),
+                  ),
+                  NavigationRailDestination(
+                    icon: MouseRegion(
+                      onEnter: (_) {
+                        setState(() {
+                          _hoveredIndex = 0;
+                        });
+                      },
+                      onExit: (_) {
+                        setState(() {
+                          _hoveredIndex = -1;
+                        });
+                      },
+                      child: Container(
+                        width: 250,
+                        decoration: BoxDecoration(
+                          color: _selectedIndex == 0
+                              ? Colors.transparent
+                              : (_hoveredIndex == 0
+                                  ? Colors.grey.withOpacity(0.15)
+                                  : Colors.transparent),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.home),
+                            const SizedBox(width: 10.0),
+                            Text(
+                              'Favorites',
+                              style: TextStyle(
+                                color: _selectedIndex == 0
+                                    ? Colors.indigo[500]
+                                    : Colors.grey,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w300,
+                                fontSize: 12,
+                                height: 1.6667,
+                              ),
+                            ),
+                            const SizedBox(width: 125.0),
+                            const Icon(
+                              Icons.arrow_forward_ios,
+                              size: 15.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    label: Text('Favorites'),
+                  ),
+                  NavigationRailDestination(
+                    icon: MouseRegion(
+                      onEnter: (_) {
+                        setState(() {
+                          _hoveredIndex = 0;
+                        });
+                      },
+                      onExit: (_) {
+                        setState(() {
+                          _hoveredIndex = -1;
+                        });
+                      },
+                      child: Container(
+                        width: 250,
+                        decoration: BoxDecoration(
+                          color: _selectedIndex == 0
+                              ? Colors.transparent
+                              : (_hoveredIndex == 0
+                                  ? Colors.grey.withOpacity(0.15)
+                                  : Colors.transparent),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.home),
+                            const SizedBox(width: 10.0),
+                            Text(
+                              'Home',
+                              style: TextStyle(
+                                color: _selectedIndex == 0
+                                    ? Colors.indigo[500]
+                                    : Colors.grey,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w300,
+                                fontSize: 12,
+                                height: 1.6667,
+                              ),
+                            ),
+                            const SizedBox(width: 125.0),
+                            const Icon(
+                              Icons.arrow_forward_ios,
+                              size: 15.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    label: Text('Documentation'),
+                  ),
                   NavigationRailDestination(
                       icon: Icon(Icons.feed), label: Text('Feed')),
                   NavigationRailDestination(
@@ -88,6 +341,18 @@ class _NavigationRailExampleState extends State<NavigationRailExample> {
                   NavigationRailDestination(
                       icon: Icon(Icons.settings), label: Text('Settings')),
                 ],
+                trailing: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(height: 300),
+                    IconButton(
+                      icon: Icon(Icons.logout),
+                      onPressed: () {
+                        // Handle logout button press
+                      },
+                    ),
+                  ],
+                ),
                 selectedIndex: _selectedIndex),
           Expanded(child: _screens[_selectedIndex]),
         ],
